60313513e224dbe91361d59d13f5ddda4e35d510
Vladimir Oltean <olteanv@gmail.com>
2018-07-08 21:43:04 +0300

    synaptics_dsx: fix invalid use of OF API resulting in boot warnings
    * Introduced in "620a688 LENOVO BSP: synaptics_dsx: backport missing dts
      properties from stock code drop", calling of_property_read_bool on DTS
      properties that are not booleans will cause the kernel to print ugly
      warning messages.
    * Functionality was somehow not affected, but the logic was incorrect.
    * Backported from the lineage-15.1 branch.
    
    Change-Id: I910ce28faa5d9d11781d20127bf21e54b83913de
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.c
+++ drivers/input/touchscreen/synaptics_dsx/synaptics_dsx_i2c.c
@@ -359,37 +359,29 @@ static int synaptics_dsx_parse_dt(struct device *dev,
 	}
 
 	/* Backported from stock kernel driver */
-	if (of_property_read_bool(np, "synaptics,power-delay-ms")) {
-		rc = of_property_read_u32(np, "synaptics,power-delay-ms",
-		                          &temp_val);
-		if (rc < 0)
-			return rc;
-		else
-			rmi4_pdata->power_delay_ms = temp_val;
-	} else {
+	rc = of_property_read_u32(np, "synaptics,power-delay-ms",
+	                         &temp_val);
+	if (rc < 0)
 		rmi4_pdata->power_delay_ms = 0;
-	}
-	/* reset gpio info */
-	if (of_property_read_bool(np, "synaptics,reset-gpio")) {
-		rmi4_pdata->reset_gpio = of_get_named_gpio_flags(np,
-		                         "synaptics,reset-gpio", 0, NULL);
-		rc = of_property_read_u32(np, "synaptics,reset-on-state",
-		                          &temp_val);
-		if (rc < 0)
-			return rc;
-		rmi4_pdata->reset_on_state = temp_val;
+	else
+		rmi4_pdata->power_delay_ms = temp_val;
 
-		rc = of_property_read_u32(np, "synaptics,reset-active-ms",
-		                          &temp_val);
-		if (rc < 0)
-			return rc;
-		rmi4_pdata->reset_active_ms = temp_val;
-	} else {
-		rmi4_pdata->reset_gpio = -1;
-	}
+	/* reset gpio info */
 	rmi4_pdata->reset_gpio = of_get_named_gpio_flags(np,
 	                         "synaptics,reset-gpio", 0,
 	                         &rmi4_pdata->reset_flags);
+
+	rc = of_property_read_u32(np, "synaptics,reset-on-state",
+	                          &temp_val);
+	if (rc < 0)
+	        return rc;
+	rmi4_pdata->reset_on_state = temp_val;
+
+	rc = of_property_read_u32(np, "synaptics,reset-active-ms",
+	                          &temp_val);
+	if (rc < 0)
+	        return rc;
+	rmi4_pdata->reset_active_ms = temp_val;
 
 	/* irq gpio info */
 	rmi4_pdata->irq_gpio = of_get_named_gpio_flags(np,
